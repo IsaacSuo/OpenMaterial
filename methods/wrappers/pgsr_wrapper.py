f9eb15ebd65703efd2dff3f0a20cd99b113e34e1
IsaacSuo <suofang000@gmail.com>
2025-12-08 18:59:20 +0800

    Adjust PGSR voxel_size to 0.005

--- methods/wrappers/pgsr_wrapper.py
+++ methods/wrappers/pgsr_wrapper.py
@@ -166,7 +166,7 @@ class PGSRMethod(BaseMethod):
         # PGSR render.py extracts mesh automatically when --skip_train is NOT set
         # Mesh is saved to {model_path}/mesh/tsdf_fusion_post.ply
         # Increase voxel_size to reduce memory usage (default 0.002 causes OOM)
-        voxel_size = config.get('voxel_size', 0.01)
+        voxel_size = config.get('voxel_size', 0.005)
         cmd = f"""python render.py \
             -s {abs_data_path} \
             -m {abs_model_path} \
